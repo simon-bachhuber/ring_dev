4727f11375416416044a26e6e9f6eb0fbf87d344
Simon Bachhuber <Simon.Bachhuberr@gmail.com>
2024-11-21 11:28:10 +0100

    adds RING's 2Seg xml model with a `rr_imp` joint to confirm that this learns the realworld arm seg2-seg3 data which contains a x-hinge joint

--- train_step1_generateData_2S.py
+++ train_step1_generateData_2S.py
@@ -1,3 +1,19 @@
+"""
+srun python ring_dev/train_step1_generateData_2S.py 24576 $BIGWORK/data/10 "['standard', 'standard-S', 'standard-S+', 'expSlow', 'expSlow-S', 'expSlow-S+', 'expFast', 'expFast-S', 'expFast-S+', 'hinUndHer', 'hinUndHer-S', 'hinUndHer-S+']" 10 --dof1 1 --dof2 0
+srun python ring_dev/train_step1_generateData_2S.py 24576 $BIGWORK/data/11 "['standard', 'standard-S', 'standard-S+', 'expSlow', 'expSlow-S', 'expSlow-S+', 'expFast', 'expFast-S', 'expFast-S+', 'hinUndHer', 'hinUndHer-S', 'hinUndHer-S+']" 11 --dof1 1 --dof2 1
+srun python ring_dev/train_step1_generateData_2S.py 24576 $BIGWORK/data/12 "['standard', 'standard-S', 'standard-S+', 'expSlow', 'expSlow-S', 'expSlow-S+', 'expFast', 'expFast-S', 'expFast-S+', 'hinUndHer', 'hinUndHer-S', 'hinUndHer-S+']" 12 --dof1 1 --dof2 2
+srun python ring_dev/train_step1_generateData_2S.py 24576 $BIGWORK/data/13 "['standard', 'standard-S', 'standard-S+', 'expSlow', 'expSlow-S', 'expSlow-S+', 'expFast', 'expFast-S', 'expFast-S+', 'hinUndHer', 'hinUndHer-S', 'hinUndHer-S+']" 13 --dof1 1 --dof2 3
+srun python ring_dev/train_step1_generateData_2S.py 24576 $BIGWORK/data/22 "['standard', 'standard-S', 'standard-S+', 'expSlow', 'expSlow-S', 'expSlow-S+', 'expFast', 'expFast-S', 'expFast-S+', 'hinUndHer', 'hinUndHer-S', 'hinUndHer-S+']" 22 --dof1 2 --dof2 2
+srun python ring_dev/train_step1_generateData_2S.py 24576 $BIGWORK/data/23 "['standard', 'standard-S', 'standard-S+', 'expSlow', 'expSlow-S', 'expSlow-S+', 'expFast', 'expFast-S', 'expFast-S+', 'hinUndHer', 'hinUndHer-S', 'hinUndHer-S+']" 23 --dof1 2 --dof2 3
+
+srun python ring_dev/train_step1_generateData_2S.py 96 $BIGWORK/data/10_val "['standard', 'standard-S', 'standard-S+', 'expSlow', 'expSlow-S', 'expSlow-S+', 'expFast', 'expFast-S', 'expFast-S+', 'hinUndHer', 'hinUndHer-S', 'hinUndHer-S+']" 101 --dof1 1 --dof2 0
+srun python ring_dev/train_step1_generateData_2S.py 96 $BIGWORK/data/11_val "['standard', 'standard-S', 'standard-S+', 'expSlow', 'expSlow-S', 'expSlow-S+', 'expFast', 'expFast-S', 'expFast-S+', 'hinUndHer', 'hinUndHer-S', 'hinUndHer-S+']" 111 --dof1 1 --dof2 1
+srun python ring_dev/train_step1_generateData_2S.py 96 $BIGWORK/data/12_val "['standard', 'standard-S', 'standard-S+', 'expSlow', 'expSlow-S', 'expSlow-S+', 'expFast', 'expFast-S', 'expFast-S+', 'hinUndHer', 'hinUndHer-S', 'hinUndHer-S+']" 121 --dof1 1 --dof2 2
+srun python ring_dev/train_step1_generateData_2S.py 96 $BIGWORK/data/13_val "['standard', 'standard-S', 'standard-S+', 'expSlow', 'expSlow-S', 'expSlow-S+', 'expFast', 'expFast-S', 'expFast-S+', 'hinUndHer', 'hinUndHer-S', 'hinUndHer-S+']" 131 --dof1 1 --dof2 3
+srun python ring_dev/train_step1_generateData_2S.py 96 $BIGWORK/data/22_val "['standard', 'standard-S', 'standard-S+', 'expSlow', 'expSlow-S', 'expSlow-S+', 'expFast', 'expFast-S', 'expFast-S+', 'hinUndHer', 'hinUndHer-S', 'hinUndHer-S+']" 221 --dof1 2 --dof2 2
+srun python ring_dev/train_step1_generateData_2S.py 96 $BIGWORK/data/23_val "['standard', 'standard-S', 'standard-S+', 'expSlow', 'expSlow-S', 'expSlow-S+', 'expFast', 'expFast-S', 'expFast-S+', 'hinUndHer', 'hinUndHer-S', 'hinUndHer-S+']" 231 --dof1 2 --dof2 3
+"""
+
 from dataclasses import replace
 from typing import Optional
 
@@ -6,7 +22,7 @@ import numpy as np
 import ring
 from ring.utils import randomize_sys
 
-sys_str = """
+sys_str1 = """
 <x_xy model="lam2">
   <options dt="0.01" gravity="0.0 0.0 9.81"/>
   <worldbody>
@@ -27,6 +43,30 @@ sys_str = """
   </worldbody>
 </x_xy>
 """
+
+sys_str2 = """
+<x_xy model="lam2">
+  <options dt="0.01" gravity="0.0 0.0 9.81"/>
+  <worldbody>
+    <body joint="free" name="seg1" pos="0.4 0.0 0.0" pos_min="0.2 -0.05 -0.05" pos_max="0.55 0.05 0.05" damping="5.0 5.0 5.0 25.0 25.0 25.0">
+      <geom pos="0.1 0.0 0.0" mass="1.0" color="dustin_exp_blue" edge_color="black" type="box" dim="0.2 0.05 0.05"/>
+      <geom pos="0.05 0.05 0.0" mass="0.1" color="black" edge_color="black" type="box" dim="0.01 0.1 0.01"/>
+      <geom pos="0.15 -0.05 0.0" mass="0.1" color="black" edge_color="black" type="box" dim="0.01 0.1 0.01"/>
+      <body joint="frozen" name="imu1" pos="0.099999994 0.0 0.035" pos_min="0.050000012 -0.05 -0.05" pos_max="0.15 0.05 0.05">
+        <geom mass="0.1" color="dustin_exp_orange" edge_color="black" type="box" dim="0.05 0.03 0.02"/>
+      </body>
+      <body joint="rr_imp" name="seg2" pos="0.20000002 0.0 0.0" pos_min="0.0 -0.05 -0.05" pos_max="0.35 0.05 0.05" damping="3.0 3.0">
+        <geom pos="0.1 0.0 0.0" mass="1.0" color="dustin_exp_white" edge_color="black" type="box" dim="0.2 0.05 0.05"/>
+        <geom pos="0.1 0.05 0.0" mass="0.1" color="black" edge_color="black" type="box" dim="0.01 0.1 0.01"/>
+        <geom pos="0.15 -0.05 0.0" mass="0.1" color="black" edge_color="black" type="box" dim="0.01 0.1 0.01"/>
+        <body joint="frozen" name="imu2" pos="0.100000024 0.0 0.035" pos_min="0.050000012 -0.05 -0.05" pos_max="0.14999998 0.05 0.05">
+          <geom mass="0.1" color="dustin_exp_orange" edge_color="black" type="box" dim="0.05 0.03 0.02"/>
+        </body>
+      </body>
+    </body>
+  </worldbody>
+</x_xy>
+"""  # noqa: E501
 
 dof_joint_types = {0: "frozen", 1: "rr", 2: "rsaddle", 3: "spherical"}
 dof_joint_dampings = {
@@ -55,16 +95,19 @@ def main(
     # sampling_rates: list[float] = [40, 60, 80, 100, 120, 140, 160, 180, 200],
     T: float = 60.0,  # 150
     motion_arti: bool = False,
-    dof1: int = 0,
-    dof2: int = 3,
+    dof1: int = None,
+    dof2: int = None,
 ):
-    sys = ring.System.create(sys_str)
-    sys = sys.change_joint_type(
-        "seg1", dof_joint_types[dof1], new_damp=dof_joint_dampings[dof1]
-    )
-    sys = sys.change_joint_type(
-        "seg2", dof_joint_types[dof2], new_damp=dof_joint_dampings[dof2]
-    )
+    sys = ring.System.create(sys_str2)
+
+    if dof1 is not None:
+        sys = sys.change_joint_type(
+            "seg1", dof_joint_types[dof1], new_damp=dof_joint_dampings[dof1]
+        )
+    if dof2 is not None:
+        sys = sys.change_joint_type(
+            "seg2", dof_joint_types[dof2], new_damp=dof_joint_dampings[dof2]
+        )
 
     ring.RCMG(
         randomize_sys.randomize_anchors(sys, anchors) if anchors else sys,
