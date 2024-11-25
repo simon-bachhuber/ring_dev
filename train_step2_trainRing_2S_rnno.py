67676150b3abe680ac65fab1c70ed81f0b1b8d31
Simon Bachhuber <Simon.Bachhuberr@gmail.com>
2024-11-25 22:56:19 +0100

    train on diodem data

--- train_step2_trainRing_2S_rnno.py
+++ train_step2_trainRing_2S_rnno.py
@@ -8,6 +8,9 @@ import qmt
 import ring
 from ring.utils import dataloader_torch
 from torch.utils.data import ConcatDataset
+from torch.utils.data import Dataset
+from torch.utils.data import WeightedRandomSampler
+import tree
 import wandb
 
 from train_support.transform_2S import Transform
@@ -50,9 +53,96 @@ def _diodem_cb(exp_id: int, net, seg1, seg2, motion_start, dof: int | None):
     )
 
 
+class TwoSegDiodemDataset(Dataset):
+    def __init__(
+        self,
+        exp_id: int,
+        seg1: str,
+        seg2: str,
+        motion_start: str,
+        transform,
+        T: float = 60,
+        Ts: float = 0.01,
+        T_offset: float = 3.0,
+    ):
+        self.N = int(T / Ts)
+        self.T = T
+        self.Ts = Ts
+        self.T_offset = T_offset
+        self.seg1 = seg1
+        self.seg2 = seg2
+        self.exp_id = exp_id
+        data = load_data(exp_id, motion_start, motion_stop=-1, resample_to_hz=1 / Ts)
+        N = data["seg1"]["quat"].shape[0]
+        self.data = {
+            "seg1": {
+                "acc": data[seg1]["imu_rigid"]["acc"],
+                "gyr": data[seg1]["imu_rigid"]["gyr"],
+                "imu_to_joint_m": np.zeros(
+                    (
+                        N,
+                        3,
+                    )
+                ),
+            },
+            "seg2": {
+                "acc": data[seg2]["imu_rigid"]["acc"],
+                "gyr": data[seg2]["imu_rigid"]["gyr"],
+                "imu_to_joint_m": np.zeros(
+                    (
+                        N,
+                        3,
+                    )
+                ),
+                "quat": qmt.qmult(qmt.qinv(data[seg1]["quat"]), data[seg2]["quat"]),
+            },
+        }
+        window = int(T_offset / Ts)
+        self.ts = list(range(0, N - self.N, window))
+        self.transform = transform
+
+    def __getitem__(self, index):
+        t = self.ts[index]
+        data = tree.map_structure(lambda a: a[t : (t + self.N)].copy(), self.data)
+        y_d = {"seg2": data["seg2"].pop("quat")}
+        return self.transform((data, y_d))
+
+    def __len__(self):
+        return len(self.ts)
+
+
 def _dof_from_path_str(path: str) -> int:
     digits = str(Path(path).name).split("_")[0]
     return sum(int(char) for char in digits)
+
+
+def _build_train_dataset_and_sampler(paths: str, transform, T, Ts, W: float = 1.0):
+    sim_ds = ConcatDataset(
+        [
+            dataloader_torch.FolderOfPickleFilesDataset(p, transform)
+            for p in paths.split(",")
+        ]
+    )
+
+    chain1 = ["seg1", "seg2", "seg3", "seg4", "seg5"]
+    chain2 = ["seg5"] + chain1[:-1]
+
+    max_exp_id = 11 if ring.ml.on_cluster() else 2
+    real_datasets = []
+    for exp_id in range(1, max_exp_id + 1):
+        chain = chain1 if exp_id < 6 else chain2
+        motion_start = 7 if exp_id == 1 else 1
+        for seg1, seg2 in zip(chain, chain[1:]):
+            ds = TwoSegDiodemDataset(exp_id, seg1, seg2, motion_start, transform, T, Ts)
+            real_datasets.append(ds)
+    real_ds = ConcatDataset(real_datasets)
+
+    print("Number of simulated datapoints: ", len(sim_ds))
+    print("Number of real-world datapoints: ", len(real_ds))
+
+    weights = [1.0] * len(sim_ds) + [len(sim_ds) * W / len(real_ds)] * len(real_ds)
+    ds = ConcatDataset([sim_ds, real_ds])
+    return ds, WeightedRandomSampler(weights, len(ds))
 
 
 def main(
@@ -80,27 +170,28 @@ def main(
     adap_clip=None,
     glob_clip=1.0,
     n_decay_episodes=None,
+    W: float = 1.0,
 ):
     np.random.seed(seed)
+
+    assert not dof
 
     if use_wandb:
         unique_id = ring.ml.unique_id()
         wandb.init(project=wandb_project, config=locals(), name=wandb_name)
 
-    transform = lambda dof: Transform(dof, rand_ori, pos, use_vqf)
+    transform = Transform(None, rand_ori, pos, use_vqf)
 
+    train_ds, sampler = _build_train_dataset_and_sampler(
+        paths, transform, 60.0, 0.01, W
+    )
     gen = dataloader_torch.dataset_to_generator(
-        ConcatDataset(
-            [
-                dataloader_torch.FolderOfPickleFilesDataset(
-                    p, transform(_dof_from_path_str(p) if dof else None)
-                )
-                for i, p in enumerate(paths.split(","))
-            ]
-        ),
+        train_ds,
         batch_size=bs,
         seed=seed,
         num_workers=num_workers,
+        sampler=sampler,
+        shuffle=False,
     )
 
     params = _params(hex(warmstart)) if warmstart else None
@@ -122,9 +213,7 @@ def main(
     callbacks = []
     for i, p in enumerate(paths.split(",")):
         path = p + "_val"
-        ds_val = dataloader_torch.FolderOfPickleFilesDataset(
-            path, transform(i + 1 if dof else None)
-        )
+        ds_val = dataloader_torch.FolderOfPickleFilesDataset(path, transform)
         X_val, y_val = dataloader_torch.dataset_to_generator(ds_val, len(ds_val))(None)
         callbacks.append(
             ring.ml.callbacks.EvalXyTrainingLoopCallback(
