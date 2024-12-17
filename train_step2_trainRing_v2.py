ff12223e9b2302fcfa62dcc3dd4110ba3aa2d516
Simon Bachhuber <Simon.Bachhuberr@gmail.com>
2024-12-17 22:54:12 +0100

    adds support for `rnno` and both `three-seg` and `four-seg` in one file

--- train_step2_trainRing_v2.py
+++ train_step2_trainRing_v2.py
@@ -1,51 +1,32 @@
 from dataclasses import replace
-import random
 
 from diodem.benchmark import benchmark
 from diodem.benchmark import IMTP
 import fire
-import jax
 import jax.numpy as jnp
 import numpy as np
 import qmt
 import ring
+from ring import maths
 from ring import ml
-from ring.utils import dataloader_torch
-from torch.utils.data import Dataset
+from ring.utils.dataloader_torch import dataset_to_generator
+from ring.utils.dataloader_torch import dataset_to_Xy
+from ring.utils.dataloader_torch import FolderOfFilesDataset
+from ring.utils.dataloader_torch import MultiDataset
+from ring.utils.dataloader_torch import ShuffledDataset
+from torch.utils.data import random_split
 import wandb
 
-lam = [-1, -1, 1, -1, 3, 4, -1, 6, 7, 8]
 
-link_names = [
-    "seg3_1Seg",
-    "seg3_2Seg",
-    "seg4_2Seg",
-    "seg3_3Seg",
-    "seg4_3Seg",
-    "seg5_3Seg",
-    "seg2_4Seg",
-    "seg3_4Seg",
-    "seg4_4Seg",
-    "seg5_4Seg",
-]
-
-dropout_rates_hard = dict(
-    seg4_2Seg=dict(imu=0.0, ja_1d=2 / 3, ja_2d=1 / 2, dof=1 / 2),
-    seg4_3Seg=dict(imu=0.5, ja_1d=2 / 3, ja_2d=1 / 2, dof=1 / 2),
-    seg5_3Seg=dict(imu=0.0, ja_1d=2 / 3, ja_2d=1 / 2, dof=1 / 2),
-    seg3_4Seg=dict(imu=0.5, ja_1d=2 / 3, ja_2d=1 / 2, dof=1 / 2),
-    seg4_4Seg=dict(imu=0.5, ja_1d=2 / 3, ja_2d=1 / 2, dof=1 / 2),
-    seg5_4Seg=dict(imu=0.0, ja_1d=2 / 3, ja_2d=1 / 2, dof=1 / 2),
-)
-
-dropout_rates_easy = dict(
-    seg4_2Seg=dict(imu=0.0, ja_1d=1 / 3, ja_2d=1 / 3, dof=0.0),
-    seg4_3Seg=dict(imu=1 / 3, ja_1d=1 / 3, ja_2d=1 / 3, dof=0.0),
-    seg5_3Seg=dict(imu=0.0, ja_1d=1 / 3, ja_2d=1 / 3, dof=0.0),
-    seg3_4Seg=dict(imu=1 / 3, ja_1d=1 / 3, ja_2d=1 / 3, dof=0.0),
-    seg4_4Seg=dict(imu=1 / 3, ja_1d=1 / 3, ja_2d=1 / 3, dof=0.0),
-    seg5_4Seg=dict(imu=0.0, ja_1d=1 / 3, ja_2d=1 / 3, dof=0.0),
-)
+def _cb_metrices_factory(warmup: int = 0):
+    return dict(
+        mae_deg=lambda q, qhat: jnp.rad2deg(
+            jnp.mean(maths.angle_error(q, qhat)[:, warmup:])
+        ),
+        incl_deg=lambda q, qhat: jnp.rad2deg(
+            jnp.mean(maths.inclination_loss(q, qhat)[:, warmup:])
+        ),
+    )
 
 
 def _params(unique_id: str = ring.ml.unique_id()) -> str:
@@ -53,30 +34,65 @@ def _params(unique_id: str = ring.ml.unique_id()) -> str:
     return home + f"params/{unique_id}.pickle"
 
 
-def _make_ring(lam, warmstart):
+def _make_net(lam, warmstart, rnn_w, rnn_d, lin_w, lin_d, layernorm, celltype, rnno):
+
     dry_run = not ring.ml.on_cluster()
-    message_dim = 400 if not dry_run else 10
-    hidden_state_dim = 600 if not dry_run else 20
-    params = _params(hex(warmstart)) if warmstart else None
-    ringnet = ml.RING(
+    if dry_run:
+        rnn_w = 10
+        rnn_d = 1
+        lin_w = 10
+        lin_d = 0
+
+    if rnno:
+        kwargs = {
+            "forward_factory": ring.ml.rnno_v1.rnno_v1_forward_factory,
+            "rnn_layers": [rnn_w] * rnn_d,
+            "linear_layers": [lin_w] * lin_d,
+            "act_fn_rnn": lambda X: X,
+            "output_dim": 16,
+        }
+    else:
+        kwargs = {
+            "hidden_state_dim": rnn_w,
+            "stack_rnn_cells": rnn_d,
+            "message_dim": lin_w,
+            "send_message_n_layers": lin_d,
+        }
+
+    net = ring.ml.RING(
+        params=_params(hex(warmstart)) if warmstart else None,
+        celltype=celltype,
         lam=lam,
-        message_dim=message_dim,
-        hidden_state_dim=hidden_state_dim,
-        params=params,
-        send_message_n_layers=0,
-        stack_rnn_cells=2,
+        layernorm=layernorm,
+        **kwargs,
     )
-    return ringnet
+    if rnno:
+        net = ring.ml.base.NoGraph_FilterWrapper(net, quat_normalize=True)
+
+    return net
 
 
-def _loss_fn_ring(q, qhat):
-    loss = jnp.array(0.0)
-    for i, p in enumerate(lam):
-        if p == -1:
-            loss += jnp.mean(ring.maths.inclination_loss(q[:, i], qhat[:, i]) ** 2)
-        else:
-            loss += jnp.mean(ring.maths.angle_error(q[:, i], qhat[:, i]) ** 2)
-    return loss / len(lam)
+class RNNO_DiodemWrapper(ring.ml.base.AbstractFilterWrapper):
+    def apply(self, X, params=None, state=None, y=None, lam=None):
+        B, T, N, F = X.shape
+        X4 = jnp.zeros((B, T, 4, F))
+        X4 = X4.at[:, :, :N].set(X)
+        yhat, state = super().apply(X4, params, state, y, lam)
+        return yhat[:, :, :N], state
+
+
+def _loss_fn_ring_factory(lam):
+    def _loss_fn_ring(q, qhat):
+        "T, N, F -> Scalar"
+        loss = jnp.array(0.0)
+        for i, p in enumerate(lam):
+            if p == -1:
+                loss += jnp.mean(ring.maths.inclination_loss(q[:, i], qhat[:, i]) ** 2)
+            else:
+                loss += jnp.mean(ring.maths.angle_error(q[:, i], qhat[:, i]) ** 2)
+        return loss / len(lam)
+
+    return _loss_fn_ring
 
 
 def _loss_fn_rnno(q, qhat):
@@ -86,41 +102,75 @@ def _loss_fn_rnno(q, qhat):
     q = q[..., :-1]
 
     loss += jnp.mean(ring.maths.inclination_loss(q[:, 0], qhat[:, 0]) ** 2)
-
-    def body_fn(i, loss):
-        q_i = jax.lax.dynamic_index_in_dim(q, i, axis=1, keepdims=False)
-        qhat_i = jax.lax.dynamic_index_in_dim(qhat, i, axis=1, keepdims=False)
-        loss += jnp.mean(ring.maths.angle_error(q_i, qhat_i) ** 2)
-        return loss
-
-    loss = jax.lax.fori_loop(jnp.array(1), M, body_fn, loss)
-    return loss / M
-
-
-def _loss_fn(q, qhat):
-    "(T, N, F) -> Scalar"
-    return _loss_fn_ring(q, qhat)
+    q, qhat = q.transpose((1, 0, 2)), qhat.transpose((1, 0, 2))
+    qhat = jnp.where(jnp.arange(4) < M, qhat, q)
+    loss += jnp.mean(ring.maths.angle_error(q[1:], qhat[1:]) ** 2) * 4 / M
+    return loss
 
 
 class Transform:
-    def __init__(self, imtp: IMTP, dropout_rates: dict):
-        self.imtp = imtp
-        self.dropout_rates = dropout_rates
+    chain = ["seg2_4Seg", "seg3_4Seg", "seg4_4Seg", "seg5_4Seg"]
+    inner = ["seg4_3Seg", "seg3_4Seg", "seg4_4Seg"]
+    lam = [-1, -1, 1, -1, 3, 4, -1, 6, 7, 8]
 
-    def __call__(self, lam2_1, lam2_2, lam3, lam4):
-        dropout_rates = self.dropout_rates
+    link_names = [
+        "seg3_1Seg",
+        "seg3_2Seg",
+        "seg4_2Seg",
+        "seg3_3Seg",
+        "seg4_3Seg",
+        "seg5_3Seg",
+        "seg2_4Seg",
+        "seg3_4Seg",
+        "seg4_4Seg",
+        "seg5_4Seg",
+    ]
+
+    def __init__(
+        self,
+        imtp: IMTP,
+        drop_imu_1d,
+        drop_imu_2d,
+        drop_imu_3d,
+        drop_ja_1d,
+        drop_ja_2d,
+        drop_dof,
+        rnno: bool,
+        three_seg: bool,
+        four_seg: bool,
+    ):
+        self.imtp = imtp
+        self.drop_imu = {1: drop_imu_1d, 2: drop_imu_2d, 3: drop_imu_3d}
+        self.drop_ja_1d = drop_ja_1d
+        self.drop_ja_2d = drop_ja_2d
+        self.drop_dof = drop_dof
+        self.rnno = rnno
+        self.three_seg = three_seg
+        self.four_seg = four_seg
+
+    def _lamX_from_lam4(self, lam4, rename_to: list[str]):
+        N = len(rename_to)
+        start = np.random.choice(list(range((5 - N))))
+        rename_from = self.chain[start : (start + N)]  # noqa: E203
+        X, y = lam4
+        for old_name, new_name in zip(rename_from, rename_to):
+            X[new_name] = X[old_name]
+            y[new_name] = y[old_name]
+        for old_name in self.chain:
+            X.pop(old_name)
+            y.pop(old_name)
+        return X, y
+
+    def __call__(self, lam41, lam42, lam43, lam44):
         imtp = self.imtp
         slices = imtp.getSlices()
+        lam = self.lam
+        link_names = self.link_names
 
-        X1, Y1 = lam2_1
-        X2, Y2 = lam2_2
-        X3, Y3 = lam3
-        X4, Y4 = lam4
-
-        X1["seg3_1Seg"] = X1.pop("seg3_2Seg")
-        X1.pop("seg4_2Seg")
-        Y1["seg3_1Seg"] = Y1.pop("seg3_2Seg")
-        Y1.pop("seg4_2Seg")
+        X1, Y1 = self._lamX_from_lam4(lam41, ["seg3_1Seg"])
+        X2, Y2 = self._lamX_from_lam4(lam42, ["seg3_2Seg", "seg4_2Seg"])
+        X3, Y3 = self._lamX_from_lam4(lam43, ["seg3_3Seg", "seg4_3Seg", "seg5_3Seg"])
+        X4, Y4 = lam44
 
         dt1 = X1.pop("dt")
         dt2 = X2.pop("dt")
@@ -133,6 +183,8 @@ class Transform:
         Y1.update(Y2)
         Y1.update(Y3)
         Y1.update(Y4)
+
+        del X2, X3, X4, Y2, Y3, Y4
 
         T = Y1["seg3_1Seg"].shape[0]
         X = np.zeros((imtp.getF(), 10, T))
@@ -147,39 +199,75 @@ class Transform:
         draw = lambda p: 1.0 - np.random.binomial(1, p=p)
 
         for i, (name, p) in enumerate(zip(link_names, lam)):
-            factor = 1.0
-            if imtp.sparse and (p != -1):
-                factor = draw(dropout_rates[name]["imu"])
-            X[slices["acc"], i] = (X1[name]["acc"].T / imtp.scale_acc) * factor
-            X[slices["gyr"], i] = (X1[name]["gyr"].T / imtp.scale_gyr) * factor
+
+            imu_factor = 1.0
+            if imtp.sparse and name in self.inner:
+                dof = int(X1[name]["dof"])
+                imu_factor = draw(self.drop_imu[dof])
+
+            X[slices["acc"], i] = (X1[name]["acc"].T / imtp.scale_acc) * imu_factor
+            X[slices["gyr"], i] = (X1[name]["gyr"].T / imtp.scale_gyr) * imu_factor
             if imtp.mag:
-                X[slices["mag"], i] = (X1[name]["mag"].T / imtp.scale_mag) * factor
+                X[slices["mag"], i] = (X1[name]["mag"].T / imtp.scale_mag) * imu_factor
+
             if p != -1:
-                if imtp.joint_axes_1d and X1[name]["dof"] == 1:
-                    X[slices["ja_1d"], i] = X1[name]["joint_params"]["rr"][
-                        "joint_axes"
-                    ][:, None] * draw(dropout_rates[name]["ja_1d"])
-                if imtp.joint_axes_2d and X1[name]["dof"] == 2:
-                    X[slices["ja_2d"], i] = X1[name]["joint_params"]["rsaddle"][
-                        "joint_axes"
-                    ].reshape(6, 1) * draw(dropout_rates[name]["ja_2d"])
+                dof = int(X1[name]["dof"])
+                if imtp.joint_axes_1d and dof == 1:
+                    X[slices["ja_1d"], i] = (
+                        X1[name]["joint_params"]["rr"]["joint_axes"][:, None]
+                        / imtp.scale_ja
+                        * draw(self.drop_ja_1d)
+                    )
+                if imtp.joint_axes_2d and dof == 2:
+                    X[slices["ja_2d"], i] = (
+                        X1[name]["joint_params"]["rsaddle"]["joint_axes"].reshape(6, 1)
+                        / imtp.scale_ja
+                        * draw(self.drop_ja_2d)
+                    )
                 if imtp.dof:
                     dof_array = np.zeros((3,))
-                    dof_array[X1[name]["dof"] - 1] = 1.0 * draw(
-                        dropout_rates[name]["dof"]
-                    )
+                    dof_array[dof - 1] = 1.0 * draw(self.drop_dof)
                     X[slices["dof"], i] = dof_array[:, None]
 
             q_p = np.array([1.0, 0, 0, 0]) if p == -1 else Y1[link_names[p]]
             q_i = Y1[name]
             Y[i] = qmt.qrel(q_p, q_i)
 
-        return X.transpose((2, 1, 0)), Y.transpose((1, 0, 2))
+        X, Y = X.transpose((2, 1, 0)), Y.transpose((1, 0, 2))
 
-    @staticmethod
-    def _rnno_padd(X, Y):
-        "X: (T, Nseg, F), Y: (T, Nseg, 4) -> (T, 4, ...)"
-        pass
+        if self.rnno:
+            X, Y = self._rnno_output_transform(X, Y)
+        else:
+            if self.four_seg:
+                M = 10
+            elif self.three_seg:
+                M = 6
+            else:
+                M = 3
+            X, Y = X[:, :M], Y[:, :M]
+
+        return X, Y
+
+    def _rnno_output_transform(self, _X, _Y):
+        "X: (T, Nseg, F), Y: (T, Nseg, 4) -> (T, 4, 5)"
+        starts = [0, 1, 3, 6]
+        if self.four_seg:
+            Ms = [1, 2, 3, 4]
+        elif self.three_seg:
+            Ms = [1, 2, 3]
+        else:
+            Ms = [1, 2]
+        M = np.random.choice(Ms)
+
+        r = slice(starts[M - 1], starts[M - 1] + M)
+
+        T = _X.shape[0]
+        F = _X.shape[-1]
+        X, Y = np.zeros((T, 4, F)), np.zeros((T, 4, 5))
+        X[:, :M] = _X[:, r]
+        Y[:, :M, :-1] = _Y[:, r]
+        X[:, :, -1] = M
+        return X, Y
 
 
 def _make_exp_callbacks(ringnet, imtp: IMTP):
@@ -193,6 +281,7 @@ def _make_exp_callbacks(ringnet, imtp: IMTP):
             motion_start=motion_start,
             filter=ringnet,
             return_cb=True,
+            cb_metrices=_cb_metrices_factory(500),
         )
         callbacks.append(cb)
         # exclude the first element because it connects to -1 and thus its `mae_deg`
@@ -216,8 +305,6 @@ def _make_exp_callbacks(ringnet, imtp: IMTP):
 
 
 def main(
-    path_lam2,
-    path_lam3,
     path_lam4,
     bs: int,
     episodes: int,
@@ -229,14 +316,52 @@ def main(
     exp_cbs: bool = False,
     lr: float = 1e-3,
     tbp: int = 1000,
-    dropout_rates: str = "hard",
+    drop_imu_1d: float = 0.75,
+    drop_imu_2d: float = 0.25,
+    drop_imu_3d: float = 0.1,
+    drop_ja_1d: float = 0.5,
+    drop_ja_2d: float = 0.5,
+    drop_dof: float = 0.0,
+    n_val: int = 256,
+    rnno: bool = False,
+    rnn_w: int = 400,
+    rnn_d: int = 2,
+    lin_w: int = 200,
+    lin_d: int = 0,
+    layernorm: bool = False,
+    celltype: str = "gru",
+    three_seg: bool = False,
+    four_seg: bool = False,
 ):
     np.random.seed(seed)
 
     if use_wandb:
         wandb.init(project=wandb_project, config=locals(), name=wandb_name)
 
-    ringnet = _make_ring(lam, warmstart)
+    if four_seg:
+        lam = Transform.lam
+        link_names = Transform.link_names
+    elif three_seg:
+        lam = [-1, -1, 1, -1, 3, 4]
+        link_names = [
+            "seg3_1Seg",
+            "seg3_2Seg",
+            "seg4_2Seg",
+            "seg3_3Seg",
+            "seg4_3Seg",
+            "seg5_3Seg",
+        ]
+    else:
+        lam = [-1, -1, 1]
+        link_names = [
+            "seg3_1Seg",
+            "seg3_2Seg",
+            "seg4_2Seg",
+        ]
+
+    net = _make_net(
+        lam, warmstart, rnn_w, rnn_d, lin_w, lin_d, layernorm, celltype, rnno
+    )
     imtp = IMTP(
         segments=None,
         sparse=True,
@@ -250,25 +375,49 @@ def main(
         scale_acc=9.81,
         scale_gyr=2.2,
         scale_dt=0.1,
+        scale_ja=0.57,
     )
-    ds_train = MultiDataset(
-        [
-            dataloader_torch.FolderOfPickleFilesDataset(p)
-            for p in [path_lam2, path_lam2, path_lam3, path_lam4]
-        ],
+    ds = MultiDataset(
+        [ShuffledDataset(FolderOfFilesDataset(p)) for p in [path_lam4] * 4],
         Transform(
-            imtp, dict(easy=dropout_rates_easy, hard=dropout_rates_hard)[dropout_rates]
+            imtp,
+            drop_imu_1d,
+            drop_imu_2d,
+            drop_imu_3d,
+            drop_ja_1d,
+            drop_ja_2d,
+            drop_dof,
+            rnno,
+            three_seg,
+            four_seg,
         ),
     )
-    generator = dataloader_torch.dataset_to_generator(
+    ds_train, ds_val = random_split(ds, [len(ds) - n_val, n_val])
+    generator = dataset_to_generator(
         ds_train,
         batch_size=bs,
         seed=seed,
         drop_last=True,
-        num_workers=None if ring.ml.on_cluster() else 2,
+        num_workers=None if ring.ml.on_cluster() else 0,
     )
+    X_val, y_val = dataset_to_Xy(ds_val)
+    if rnno:
+        y_val = y_val[..., :-1]
+    callbacks = [
+        ring.ml.callbacks.EvalXyTrainingLoopCallback(
+            net,
+            _cb_metrices_factory(),
+            X_val,
+            y_val,
+            lam,
+            "val",
+            link_names=link_names,
+        )
+    ]
 
-    callbacks = _make_exp_callbacks(ringnet, imtp) if exp_cbs else []
+    if exp_cbs:
+        net_diodem = RNNO_DiodemWrapper(net) if rnno else net
+        callbacks.extend(_make_exp_callbacks(net_diodem, imtp))
 
     n_decay_episodes = 9500
     optimizer = ring.ml.make_optimizer(
@@ -278,7 +427,7 @@ def main(
     ml.train_fn(
         generator,
         episodes,
-        ringnet,
+        net,
         optimizer=optimizer,
         callbacks=callbacks,
         callback_kill_if_nan=True,
@@ -288,33 +437,9 @@ def main(
         seed_network=seed,
         link_names=link_names,
         tbp=tbp,
-        loss_fn=_loss_fn,
+        loss_fn=_loss_fn_rnno if rnno else _loss_fn_ring_factory(lam),
+        metrices=None,
     )
-
-
-class MultiDataset(Dataset):
-    def __init__(self, datasets, transform=None):
-        """
-        Args:
-            datasets: A list of datasets to sample from.
-            transform: A function that takes N items (one from each dataset) and combines them.
-        """  # noqa: E501
-        self.datasets = datasets
-        self.transform = transform
-
-    def __len__(self):
-        # Length is defined by the smallest dataset in the list
-        return min(len(ds) for ds in self.datasets)
-
-    def __getitem__(self, idx):
-        # Randomly sample one element from each dataset
-        sampled_items = [ds[random.randint(0, len(ds) - 1)] for ds in self.datasets]
-
-        if self.transform:
-            # Apply the transformation to all sampled items
-            return self.transform(*sampled_items)
-
-        return tuple(sampled_items)
 
 
 if __name__ == "__main__":
